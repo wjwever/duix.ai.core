3b0504512f4c2965bb5426d168482e925fa427c9
wjwever <1216451203@qq.com>
2025-11-13 16:16:01 +0800

    update

--- cmake/websocketpp.cmake
+++ cmake/websocketpp.cmake
@@ -1,36 +1,36 @@
 function(download_websocketpp)
   include(FetchContent)
 
-  set(websocketpp_URL   "https://github.com/zaphoyd/websocketpp/archive/refs/tags/0.8.2.tar.gz")
-  #set(websocketpp_URL2  "https://github.com/libsndfile/websocketpp")
-  #set(websocketpp_HASH "SHA256=37c1aa230b00fe062791d800d8fc50aa3de215918d3dce6440699e67275d859e")
-
-  #set(websocketpp_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-  #set(websocketpp_BUILD_PYTHON OFF CACHE BOOL "" FORCE)
-  #set(websocketpp_ENABLE_CHECK OFF CACHE BOOL "" FORCE)
+  # The latest commit on the develop branch os as 2022-10-22
+  set(websocketpp_URL  "https://github.com/zaphoyd/websocketpp/archive/b9aeec6eaf3d5610503439b4fae3581d9aff08e8.zip")
+  set(websocketpp_URL2  "https://hf-mirror.com/csukuangfj/sherpa-onnx-cmake-deps/resolve/main/websocketpp-b9aeec6eaf3d5610503439b4fae3581d9aff08e8.zip")
+  set(websocketpp_HASH "SHA256=1385135ede8191a7fbef9ec8099e3c5a673d48df0c143958216cd1690567f583")
 
   # If you don't have access to the Internet,
   # please pre-download websocketpp
-  #set(possible_file_locations
-  #  $ENV{HOME}/Downloads/websocketpp-1.21.1.tar.gz
-  #  ${CMAKE_SOURCE_DIR}/websocketpp-1.21.1.tar.gz
-  #  ${CMAKE_BINARY_DIR}/websocketpp-1.21.1.tar.gz
-  #  /tmp/websocketpp-1.21.1.tar.gz
-  #  /star-fj/fangjun/download/github/websocketpp-1.21.1.tar.gz
-  #)
+  set(possible_file_locations
+    $ENV{HOME}/Downloads/websocketpp-b9aeec6eaf3d5610503439b4fae3581d9aff08e8.zip
+    ${CMAKE_SOURCE_DIR}/websocketpp-b9aeec6eaf3d5610503439b4fae3581d9aff08e8.zip
+    ${CMAKE_BINARY_DIR}/websocketpp-b9aeec6eaf3d5610503439b4fae3581d9aff08e8.zip
+    /tmp/websocketpp-b9aeec6eaf3d5610503439b4fae3581d9aff08e8.zip
+    /star-fj/fangjun/download/github/websocketpp-b9aeec6eaf3d5610503439b4fae3581d9aff08e8.zip
+  )
 
-  #foreach(f IN LISTS possible_file_locations)
-  #  if(EXISTS ${f})
-  #    set(websocketpp_URL  "${f}")
-  #    file(TO_CMAKE_PATH "${websocketpp_URL}" websocketpp_URL)
-  #    message(STATUS "Found local downloaded websocketpp: ${websocketpp_URL}")
-  #    set(websocketpp_URL2 )
-  #    break()
-  #  endif()
-  #endforeach()
+  foreach(f IN LISTS possible_file_locations)
+    if(EXISTS ${f})
+      set(websocketpp_URL  "${f}")
+      file(TO_CMAKE_PATH "${websocketpp_URL}" websocketpp_URL)
+      message(STATUS "Found local downloaded websocketpp: ${websocketpp_URL}")
+      set(websocketpp_URL2)
+      break()
+    endif()
+  endforeach()
 
   FetchContent_Declare(websocketpp
-    URL ${websocketpp_URL}
+    URL
+      ${websocketpp_URL}
+      ${websocketpp_URL2}
+    URL_HASH          ${websocketpp_HASH}
   )
 
   FetchContent_GetProperties(websocketpp)
@@ -39,26 +39,8 @@ function(download_websocketpp)
     FetchContent_Populate(websocketpp)
   endif()
   message(STATUS "websocketpp is downloaded to ${websocketpp_SOURCE_DIR}")
-  message(STATUS "websocketpp's binary dir is ${websocketpp_BINARY_DIR}")
+  # add_subdirectory(${websocketpp_SOURCE_DIR} ${websocketpp_BINARY_DIR} EXCLUDE_FROM_ALL)
   include_directories(${websocketpp_SOURCE_DIR})
-
-  #if(BUILD_SHARED_LIBS)
-  #  set(_build_shared_libs_bak ${BUILD_SHARED_LIBS})
-  #  set(BUILD_SHARED_LIBS OFF)
-  #endif()
-
-  #add_subdirectory(${websocketpp_SOURCE_DIR} ${websocketpp_BINARY_DIR} EXCLUDE_FROM_ALL)
-
-  #if(_build_shared_libs_bak)
-  #  set_target_properties(websocketpp-core
-  #    PROPERTIES
-  #      POSITION_INDEPENDENT_CODE ON
-  #      C_VISIBILITY_PRESET hidden
-  #      CXX_VISIBILITY_PRESET hidden
-  #  )
-  #  set(BUILD_SHARED_LIBS ON)
-  #endif()
-
 endfunction()
 
 download_websocketpp()
